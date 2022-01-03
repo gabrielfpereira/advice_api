a3a41faccef02896313c7ce094fe21c454a25e85
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-01-03 14:50:02 -0300

    Alterar nome da resposta de slip para advice

--- resources/views/advice-home.blade.php
+++ resources/views/advice-home.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styleSlipHome.css">
+    <link rel="stylesheet" href="{{ asset('css/advice.css') }}">
     <title>Gerador de Conselho</title>
 </head>
 <body>
